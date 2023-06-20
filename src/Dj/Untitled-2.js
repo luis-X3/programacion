13d79c3b6d8f17b2f53f546738b45a951a7c121f
Luis <luisangdelossantos23@gmail.com>
2023-06-20 02:29:34 -0400

    commit inicial

---
+++ src/Dj/Untitled-2.js
@@ -0,0 +1,16 @@
+
+var variable1 = "hola me llamo luis"
+var edad = "260"
+
+//esta variable va mantener el valor de la sumatoria 
+var sumatoria = "Luis tiene " + edad + " años"
+console.log(sumatoria)
+
+//esto es un comentario. Esto no va afectar la compilacion ni va tener ninguna efecto en la programcion
+
+
+var edadPersona = 25
+edadPersona = 26
+
+
+console.log(edadPersona)
